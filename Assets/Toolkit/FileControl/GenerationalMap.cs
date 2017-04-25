22ff191a84ece8dc0689bdf07ad1fb56117c9a6d
Cooper Davies <ZigZalgo@users.noreply.github.com>
2017-04-24 23:14:34 -0600

    Delete GenerationalMap.cs

--- Assets/Toolkit/FileControl/GenerationalMap.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-public class GenerationalMap : SerializationContainers
-{
-    public GenerationalMap(BlockManager man, IndividualManager indiv, MutationManager mut, List<Individual> container) : base(man, indiv, mut, container) { }
-}
